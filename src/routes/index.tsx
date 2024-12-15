db98e315e9fe515f7a8cd28589c84a0e990d9f0b
afzalahamad <kingsolo2099@gmail.com>
2024-12-15 13:58:16 +0000

    Inital test commit for syre date

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -6,7 +6,7 @@ export default component$(() => {
     <>
       <h1>Hi 👋</h1>
       <div>
-        Can't wait to see what you build with qwik!
+        Wecolme to Syre AI
         <br />
         Happy coding.
       </div>
@@ -15,7 +15,7 @@ export default component$(() => {
 });
 
 export const head: DocumentHead = {
-  title: "Welcome to Qwik",
+  title: "Syre AI Official",
   meta: [
     {
       name: "description",
